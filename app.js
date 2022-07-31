28d3e6061edd582b78c4a801a7592e5994749b38
Fernanda <69811489+renanfer721@users.noreply.github.com>
2022-07-30 21:05:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let button = document.getElementById('button');
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+
+function somarnumeros(numero1,numero2){
+    return numero1 + numero2
+}
+
+button .addEventListener ('click',[] =>{
+console.log(somarnumeros(input1.value , input2.value));
+})
